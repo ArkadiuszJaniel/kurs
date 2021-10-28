b6e8e2eea9a31487ec80ec1e14a9135e3cc41ab0
Arkadiusz Janiel <arkadiuszjaniel@gmail.com>
2021-10-28 17:14:11 +0200

    ostatni1

--- App1/App1/Program.cs
+++ App1/App1/Program.cs
@@ -18,6 +18,7 @@ namespace App1
             Console.WriteLine("Hello World!");
             Console.WriteLine("Hello World!");
             Console.WriteLine("Hello World!");
+            72c8c143552760d26510e4bd224f7efabd14fc5972c8c143552760d26510e4bd224f7efabd14fc59
 
             Console.WriteLine("Hello World!");
             Console.WriteLine("Hello World!");
