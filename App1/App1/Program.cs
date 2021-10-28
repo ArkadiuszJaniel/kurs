df8ebb4d42376824c1f21cfa2bd2d39fa06949f0
Arkadiusz Janiel <arkadiuszjaniel@gmail.com>
2021-10-28 17:03:42 +0200

    ostattni commit

--- App1/App1/Program.cs
+++ App1/App1/Program.cs
@@ -18,6 +18,8 @@ namespace App1
             Console.WriteLine("Hello World!");
             Console.WriteLine("Hello World!");
             Console.WriteLine("Hello World!");
+
+            Console.WriteLine("Hello World!");
             Console.WriteLine("Hello World!");
 
             Console.ReadKey();
